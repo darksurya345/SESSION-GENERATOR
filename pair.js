c3033ad4ccd8e89da42dc3cc24542da6bdff1f6e
darksurya345 <maityrohit253@gmail.com>
2026-03-20 01:08:01 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
 
                let GIFTED_MD_TEXT = `
-*_Pair Code Connected by WASI TECH*
+*_Pair Code Connected by SURYA*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『 WOW YOU'VE CHOSEN SURYA-X 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║❒ *Ytube:* _youtube.com/XXXXX_
+║❒ *Owner:* _https://wa.me/917797099719_
+║❒ *Repo:* _https://github.com/darksurya345/SURYA-X
+║❒ *WaGroup:* _https://whatsapp.com/channel/0029VbBvVG8AYlUCU47u4W1t
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbBvVG8AYlUCU47u4W1t
+║❒ *Plugins:* _https://github.com/darksurya345/SURYA-X-PLUGINS_
 ╚════════════════════════╝
 _____________________________________
 
